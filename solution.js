b1988c3909717ca307800ca120dba5cef319313d
S Rabi <rabi.sanaye@yahoo.com>
2022-07-05 13:25:54 +0200

    first exercise

---
+++ solution.js
@@ -0,0 +1,11 @@
+// 1:
+let introSentence = "hello, my name is Fran and I am";
+let age = 25;
+console.log(introSentence + " " + age);
+console.log(introSentence.concat(" ", age));
+// 2:
+const integer = "1005";
+console.log(Number(integer));
+// 2: second part
+const float = "10.05";
+console.log(Number(float));
